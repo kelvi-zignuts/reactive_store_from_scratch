2469ea235f371dba813143d349615d414cd4a5bb
kelvi-zignuys <kelvib@zignuts.com>
2024-06-17 17:24:46 +0530

    implement of directives - loops with v-for, binding with v-bind and events and reactivity for display currect selected value

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
+import {ref} from 'vue'
+
+const periods = ["Today", "This Week", "This Month"];
+
+let selectedPeriod = ref("Today");
+
+function selectPeriod(period: string) {
+  console.log(period);
+  selectedPeriod.value = period;
+}
 </script>
 
 <template>
-    <nav class="is-primary panel">
-        <span class="panel-tabs">
-            <a>Today</a>
+  <nav class="is-primary panel">
+    {{ selectedPeriod }}
+    <span class="panel-tabs">
+      <!-- <a>Today</a>
             <a>This Week</a>
-            <a>This Month</a>
-        </span>
-    </nav>
+            <a>This Month</a> -->
+      <a
+        v-for="period in periods"
+        :key="period"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
+  </nav>
 </template>
