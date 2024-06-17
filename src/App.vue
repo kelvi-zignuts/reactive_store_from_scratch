d3094a4671cba93b8520b456e88e6f7dd75121af
kelvi-zignuys <kelvib@zignuts.com>
2024-06-17 16:35:20 +0530

    create vue.js project with vite and typescript and in that first working on panel-tabs in home page(vue single file Components) using bulma css

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TimeLine from './components/TimeLine.vue'
+</script>
+
+<template>
+  <div class="section">
+    <div class="container">
+      <TimeLine/>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css";
+</style>
